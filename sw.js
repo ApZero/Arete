8de5f7f70e883489d67499d542bb2b5ff40be0e2
ApZero <stefan_kufeld@hotmail.com>
2026-07-04 10:01:16 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string on every deploy to force clients to refresh cached assets.
-const CACHE_VERSION = 'arete-v2';
+const CACHE_VERSION = 'arete-v3';
 
 const ASSETS = [
   './',
